ae4cd59ed256b9e6e5f521e0493a3e31343d1895
Emilio Cuenca <emilio.sebastianc@gmail.com>
2026-09-24 12:28:00 -0500

    v1

--- Talleres/Taller2/main.go
+++ Talleres/Taller2/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "converter/monedas"
+import (
+	"converter/monedas"
+	"converter/vocales"
+	"fmt"
+)
 
 func main() {
-	monedas.Menu()
+	opt := 0
+	fmt.Println("1 para Conversor de monedas, 2 para contador de vocales y 0 para salir")
+	fmt.Scan(&opt)
+	switch opt {
+	case 0:
+		break
+	case 1:
+		monedas.Menu()
+		main()
+	case 2:
+		vocales.Contar()
+		main()
+	default:
+		main()
+	}
 }
--- Talleres/Taller2/vocales/vocales.go
+++ Talleres/Taller2/vocales/vocales.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func Contar() {
 	frase := ""
+	var voA, voE, voI, voO, voU int
+	fmt.Println("Escribe una frase para contar sus vocales: ")
+	fmt.Scan(&frase)
+	for _, v := range frase {
+		switch v {
+		case 'a', 'A':
+			voA++
+		case 'e', 'E':
+			voE++
+		case 'i', 'I':
+			voI++
+		case 'o', 'O':
+			voO++
+		case 'u', 'U':
+			voU++
+		}
+	}
 	fmt.Println("La frase que pusiste tiene un total de: ")
 	fmt.Println(voA, " As")
 	fmt.Println(voE, " Es")
